8bbb7e264b4dcd7ae3008e79090497f9721c154c
jaypee15 <okoyejohnpaul15@gmail.com>
2023-05-05 21:38:13 +0100

    removed indentation error

--- voice_assistant.py
+++ voice_assistant.py
@@ -89,210 +89,208 @@ def takeCommand():
   
 
 
-
 if __name__ == '__main__':
-	clear = lambda: os.system('cls')
-	
-	# This Function will clean any
-	# command before execution of this python file
-	clear()
-	wishMe()
-	username()
-	
-	while True:
-		
-		query = takeCommand().lower()
-		
-		# All the commands said by user will be
-		# stored here in 'query' and will be
-		# converted to lower case for easily
-		# recognition of command
-		if 'wikipedia' in query:
-			speak('Searching Wikipedia...')
-			query = query.replace("wikipedia", "")
-			results = wikipedia.summary(query, sentences = 3)
-			speak("According to Wikipedia")
-			print(results)
-			speak(results)
+    clear = lambda: os.system('cls')
 
-		elif 'open youtube' in query:
-			speak("Opening Youtube\n")
-			webbrowser.open("youtube.com")
+    # This Function will clean any
+    # command before execution of this python file
+    clear()
+    wishMe()
+    username()
+    
+    while True:
+        
+        query = takeCommand().lower()
+        
+        
+        
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences = 3)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
 
-		elif 'open google' in query:
-			speak("Opening Google\n")
-			webbrowser.open("google.com")
+        elif 'open youtube' in query:
+            speak("Opening Youtube\n")
+            webbrowser.open("youtube.com")
 
-		elif 'open stackoverflow' in query:
-			speak("Opening stackoverflow .Happy coding")
-			webbrowser.open("stackoverflow.com")
+        elif 'open google' in query:
+            speak("Opening Google\n")
+            webbrowser.open("google.com")
 
-		elif 'play music' in query or "play song" in query:
-			speak("Listen to some music")
-			
-			music_dir = "C:\JAYPEE\MUSIC"
-			songs = os.listdir(music_dir)
-			print(songs)
-			random = os.startfile(os.path.join(music_dir, songs[1]))
+        elif 'open stackoverflow' in query:
+            speak("Opening stackoverflow .Happy coding")
+            webbrowser.open("stackoverflow.com")
 
-		elif 'the time' in query:
-			strTime = datetime.datetime.now().strftime("% H:% M:% S")
-			speak(f"The time is {strTime}")
+        elif 'play music' in query or "play song" in query:
+            speak("Listen to some music")
+            
+            music_dir = "C:\JAYPEE\MUSIC"
+            songs = os.listdir(music_dir)
+            print(songs)
+            random = os.startfile(os.path.join(music_dir, songs[1]))
 
-		
-		elif 'how are you' in query:
-			speak("I am fine, Thank you")
-			speak("How are you, Sir")
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"The time is {strTime}")
 
-		elif 'fine' in query or "good" in query:
-			speak("It's good to know that your fine")
+        
+        elif 'how are you' in query:
+            speak("I am fine, Thank you")
+            speak("How are you, Sir")
 
-		elif "change my name to" in query:
-			query = query.replace("change my name to", "")
-			boss_name = query
+        elif 'fine' in query or "good" in query:
+            speak("It's good to know that your fine")
+
+        elif "change my name to" in query:
+            query = query.replace("change my name to", "")
+            boss_name = query
 
         elif "change name" in query:
-			speak("What would you like to call me, Sir ")
-			assistant_name = takeCommand()
-			speak("Thanks for naming me", assistant_name)
-           
+            speak("What would you like to call me, Sir ")
+            assistant_name = takeCommand()
+            speak("Thanks for naming me")
+            speak(assistant_name)
+            
 
-		elif "what's your name" in query or "What is your name" in query:
-			speak("My friends call me")
-			speak(assistant_name)
-			print("My friends call me", assistant_name)
+        elif "what's your name" in query or "What is your name" in query:
+            speak("My friends call me")
+            speak(assistant_name)
+            print("My friends call me", assistant_name)
 
-		elif 'exit' in query:
-			speak("Thanks for giving me your time")
-			exit()
+        elif 'exit' in query:
+            speak("Thanks for giving me your time")
+            exit()
 
-		elif "who made you" in query or "who created you" in query:
-			speak("I have been created by Johnpaul.")
-			
-		elif 'joke' in query:
-			speak(pyjokes.get_joke())
-			
+        elif "who made you" in query or "who created you" in query:
+            speak("I have been created by Johnpaul.")
+            
+        elif 'joke' in query:
+            speak(pyjokes.get_joke())
+            
 
-		elif 'search' in query:
-			
-			query = query.replace("search", "")
-			query = query.replace("play", "")		
-			webbrowser.open(query)
+        elif 'search' in query:
+            
+            query = query.replace("search", "")
+            query = query.replace("play", "")		
+            webbrowser.open(query)
 
 
-		elif "why have you come to this world" in query:
-			speak("I cannot tell you my secret")
+        elif "why have you come to this world" in query:
+            speak("I cannot tell you my secret")
 
-		elif 'power point presentation' in query:
-			speak("opening Power Point presentation")
-			power = r"C:\\Users\\user\\Documents\\ME\\Voice Assistant.pptx"
-			os.startfile(power)
+        elif 'power point presentation' in query:
+            speak("opening Power Point presentation")
+            power = r"C:\\Users\\user\\Documents\\ME\\Voice Assistant.pptx"
+            os.startfile(power)
 
-		elif "who are you" in query:
-			speak("I am your virtual assistant")
+        elif "who are you" in query:
+            speak("I am your virtual assistant")
 
-		
-		elif 'lock window' in query:
-				speak("locking the device")
-				ctypes.windll.user32.LockWorkStation()
+        
+        elif 'lock windows' in query:
+                speak("locking the device")
+                ctypes.windll.user32.LockWorkStation()
 
-		elif 'shutdown system' in query:
-				speak("Hold On a Sec ! Your system is on its way to shut down")
-				subprocess.call('shutdown / p /f')
-				
-		elif 'empty recycle bin' in query:
-			winshell.recycle_bin().empty(confirm = False, show_progress = False, sound = True)
-			speak("Recycle Bin Recycled")
+        elif 'shutdown system' in query:
+                speak("Hold On a Sec ! Your system is on its way to shut down")
+                subprocess.call('shutdown / p /f')
+                
+        elif 'empty recycle bin' in query:
+            winshell.recycle_bin().empty(confirm = False, show_progress = False, sound = True)
+            speak("Recycle Bin Recycled")
 
-		elif "don't listen" in query or "stop listening" in query:
-			speak("for how long do you want to stop jaypee 1 point o from listening for commands?")
-			a = int(takeCommand())
-			time.sleep(a)
-			print(a)
+        elif "don't listen" in query or "stop listening" in query:
+            speak("for how long do you want to stop jaypee 1 point o from listening for commands?")
+            a = int(takeCommand())
+            time.sleep(a)
+            print(a)
 
-		elif "where is" in query:
-			query = query.replace("where is", "")
-			location = query
-			speak("User asked to Locate")
-			speak(location)
-			webbrowser.open("https://www.google.nl / maps / place/" + location + "")
+        elif "where is" in query:
+            query = query.replace("where is", "")
+            location = query
+            speak("User asked to Locate")
+            speak(location)
+            webbrowser.open("https://www.google.nl / maps / place/" + location + "")
 
-		elif "camera" in query or "take a photo" in query:
-			ec.capture(0, "Jaypee Camera ", "img.jpg")
+        elif "camera" in query or "take a photo" in query:
+            ec.capture(0, "Jaypee Camera ", "img.jpg")
 
-		elif "restart" in query:
-			subprocess.call(["shutdown", "/r"])
-			
-		elif "hibernate" in query or "sleep" in query:
-			speak("Hibernating")
-			subprocess.call("shutdown / h")
+        elif "restart" in query:
+            subprocess.call(["shutdown", "/r"])
+            
+        elif "hibernate" in query or "sleep" in query:
+            speak("Hibernating")
+            subprocess.call("shutdown / h")
 
-		elif "log off" in query or "sign out" in query:
-			speak("Make sure all the application are closed before sign-out")
-			time.sleep(5)
-			subprocess.call(["shutdown", "/l"])
+        elif "log off" in query or "sign out" in query:
+            speak("Make sure all the application are closed before sign-out")
+            time.sleep(5)
+            subprocess.call(["shutdown", "/l"])
 
-		elif "write a note" in query:
-			speak("What should i write, sir")
-			note = takeCommand()
-			file = open('jaypee.txt', 'w')
-			speak("Sir, Should i include date and time?")
-			snfm = takeCommand()
-			if 'yes' in snfm or 'sure' in snfm:
-				strTime = datetime.datetime.now().strftime("% H:% M:% S")
-				file.write(strTime)
-				file.write(" :- ")
-				file.write(note)
-			else:
-				file.write(note)
-		
-		elif "show note" in query:
-			speak("Showing Notes")
-			file = open("jarvis.txt", "r")
-			print(file.read())
-			speak(file.read(6))
+        elif "write a note" in query:
+            speak("What should i write, sir")
+            note = takeCommand()
+            file = open('jaypee.txt', 'w')
+            speak("Sir, Should i include date and time?")
+            snfm = takeCommand()
+            if 'yes' in snfm or 'sure' in snfm:
+                strTime = datetime.datetime.now().strftime("% H:% M:% S")
+                file.write(strTime)
+                file.write(" :- ")
+                file.write(note)
+            else:
+                file.write(note)
+        
+        elif "show note" in query:
+            speak("Showing Notes")
+            file = open("jarvis.txt", "r")
+            print(file.read())
+            speak(file.read(6))
 
-		
-					
-		# NPPR9-FWDCX-D2C8J-H872K-2YT43
-		elif "jaypee" in query:
-			
-			wishMe()
-			speak("Jaypee 1 point o at your service Mister")
-			speak(assistant_name)
+        
+                    
+        # NPPR9-FWDCX-D2C8J-H872K-2YT43
+        elif "jaypee" in query:
+            
+            wishMe()
+            speak("Jaypee 1 point o at your service Mister")
+            speak(assistant_name)
 
-		elif "weather" in query:
-			
-			# Google Open weather website
-			# to get API of Open weather
-			api_key = "Api key"
-			base_url = "http://api.openweathermap.org / data / 2.5 / weather?"
-			speak(" City name ")
-			print("City name : ")
-			city_name = takeCommand()
-			complete_url = base_url + "appid =" + api_key + "&q =" + city_name
-			response = requests.get(complete_url)
-			x = response.json()
-			
-			if x["code"] != "404":
-				y = x["main"]
-				current_temperature = y["temp"]
-				current_pressure = y["pressure"]
-				current_humidiy = y["humidity"]
-				z = x["weather"]
-				weather_description = z[0]["description"]
-				print(" Temperature (in kelvin unit) = " +str(current_temperature)+"\n atmospheric pressure (in hPa unit) ="+str(current_pressure) +"\n humidity (in percentage) = " +str(current_humidiy) +"\n description = " +str(weather_description))
-			
-			else:
-				speak(" City Not Found ")
-			
+        elif "weather" in query:
+            
+            # Google Open weather website
+            # to get API of Open weather
+            api_key = "Api key"
+            base_url = "http://api.openweathermap.org / data / 2.5 / weather?"
+            speak(" City name ")
+            print("City name : ")
+            city_name = takeCommand()
+            complete_url = base_url + "appid =" + api_key + "&q =" + city_name
+            response = requests.get(complete_url)
+            x = response.json()
+            
+            if x["code"] != "404":
+                y = x["main"]
+                current_temperature = y["temp"]
+                current_pressure = y["pressure"]
+                current_humidiy = y["humidity"]
+                z = x["weather"]
+                weather_description = z[0]["description"]
+                print(" Temperature (in kelvin unit) = " +str(current_temperature)+"\n atmospheric pressure (in hPa unit) ="+str(current_pressure) +"\n humidity (in percentage) = " +str(current_humidiy) +"\n description = " +str(weather_description))
+            
+            else:
+                speak(" City Not Found ")
+            
 
-		elif "wikipedia" in query:
-			webbrowser.open("wikipedia.com")
+        elif "wikipedia" in query:
+            webbrowser.open("wikipedia.com")
 
-		elif "Good Morning" in query:
-			speak("A warm" +query)
-			speak("How are you Mister")
-			speak(assistant_name)
+        elif "Good Morning" in query:
+            speak("A warm" +query)
+            speak("How are you Mister")
+            speak(assistant_name)
 
-		
+        
